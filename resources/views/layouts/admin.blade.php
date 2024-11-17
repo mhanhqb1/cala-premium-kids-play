9c97b8c81e9a2e655909979d213b2cdea72d1754
AnhMH <anh.mh@moziasoft.com>
2024-11-17 11:24:47 +0700

    Add menu pos

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,6 +18,11 @@
                 <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.pos.index') }}" class="nav-link {{ Request::is('admin/pos*') ? 'active' : '' }}">
+                        POS
+                    </a>
+                </li>
             </ul>
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
